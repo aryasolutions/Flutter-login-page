2c227fc378a72ea39a2b33aae3f82ab1763bfca7
aryasolutions <aryaitsol@gmail.com>
2021-05-26 00:29:40 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,41 +6,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Arya Solutions',
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Welcome to Arya Solutions'),
         ),
         body: Center(
-          child: Container(
-              width: 300,
-              height: 300,
-              color: Colors.amberAccent,
-              child: Center(
-                child: Container(
-                     width: 280,
-                  child: Column(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Container(
+                decoration: new BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: new BorderRadius.only(
+                      topLeft: const Radius.circular(40.0),
+                      topRight: const Radius.circular(40.0),
+                      bottomRight: const Radius.circular(40.0),
+                      bottomLeft: const Radius.circular(40.0),
+                    )),
+                width: 300,
+                height: 260,
+                child: Center(
+                  child: Container(
+                    width: 280,
+                    child: Column(
                       children: [
-                    SizedBox(height: 10),
-                          Text('Login Page'),
-                              SizedBox(height: 10),
+                        SizedBox(height: 30),
+                        Text('Login Page'),
+                        SizedBox(height: 25),
                         TextField(
                           decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "username"
-                            ),
+                              border: OutlineInputBorder(),
+                              hintText: "username"),
                         ),
-                            SizedBox(height: 10),
-                           TextField(
+                        SizedBox(height: 10),
+                        TextField(
                           decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "Password"
-                            ),
+                              border: OutlineInputBorder(),
+                              hintText: "Password"),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('TextButton'),
                         )
                       ],
                     ),
+                  ),
                 ),
-            ),
+              ),
+            ],
           ),
         ),
       ),
